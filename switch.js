3506be709d1e67708d286373ed69ad6274411548
serhiicss <serhii.css@gmail.com>
2016-01-30 13:06:45 +0300

    More switch examples

--- switch.js
+++ switch.js
@@ -27,3 +27,45 @@ switch (val) {
     return "delta";
 }
 //will return "gamma"
+
+// ----------------------------------------
+// ----------------------------------------
+var myTest = 5;
+
+function myTest(val) {
+  var answer = "";
+  switch(val) {
+    case 1 :
+    case 2 :
+    case 3 :
+      return "Low";
+    case 4 :
+    case 5 :
+    case 6 :
+      return "Mid";
+    case 7 :
+    case 8 :
+    case 9 :
+      return "High";
+  }
+
+// will reutrn "Mid"
+
+
+switch (val) {
+  case "bob" :
+    return "Marley";
+    break;
+  case 42 :
+    return "The Answer";
+    break;
+  case 1:
+    return "There is no #1";
+    break;
+  case 99 :
+    return "Missed me by this much!";
+    break;
+  case 7:
+    return "Ate Nine";
+    break;
+}
