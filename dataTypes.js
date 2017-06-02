de42b7352473c453e74a388409c9cdb9fe895064
Serhii Borovskyi <serhii.css@gmail.com>
2017-06-02 13:59:05 +0300

    Update dataTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -1,2 +1,8 @@
 // In computer science, data is anything that is meaningful to the computer. JavaScript provides seven different data types which are 
-undefined, null, boolean, string, symbol, number, and object.
+undefined, 
+null,
+boolean,
+string,
+symbol,
+number,
+object.
